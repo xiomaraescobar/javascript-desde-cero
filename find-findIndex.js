84404cd7ace5139a6fe7641a41dd05d0e49ee2f5
Your Name <you@example.com>
2024-07-05 09:27:35 -0500

    find y findIndex

--- find-findIndex.js
+++ find-findIndex.js
@@ -19,32 +19,32 @@ console.log(indexNumber)
 
 // Exercise: Raffle Winner Verification Program
 
-const winningParticipants = [
-  { id: 1, name: 'Jennifer', ticketNumber: 001 },
-  { id: 8, name: 'Michael', ticketNumber: 008 },
-  { id: 15, name: 'Emily', ticketNumber: 015 },
-  { id: 47, name: 'Charlie', ticketNumber: 047 }
+const winningParticipants = [ //lista de participantes creamos un array en un objetos para almacenar los datos de cada persona
+  { id: 1, name: 'Camila', ticketNumber: 001 },
+  { id: 8, name: 'Manuela', ticketNumber: 008 },
+  { id: 15, name: 'David', ticketNumber: 015 },
+  { id: 47, name: 'Marcela', ticketNumber: 047 }
 ]
 
-function findWinnerByName (name) {
-  const winner = winningParticipants.find(participants => participants.name === name)
-  return winner || 'No winner found with that name.'
+function findWinnerByName (name) { //funcion para verificar si exite el nombre del participantes
+  const winner = winningParticipants.find(participants => participants.name === name)//aqui decimos si el nombre que pasamos es igual al nombre entonces hay un ganador
+  return winner || 'No winner found with that name.' //sino nos envia un mensaje de no es el ganador
 }
 
-function findIndexWinnerByTicket (ticketNumber) {
+function findIndexWinnerByTicket (ticketNumber) { //verificacion
   const index = winningParticipants.findIndex(participants => participants.ticketNumber === ticketNumber)
-  return index !== -1 ? index : 'No winner found with that ticket number.'
+  return index !== -1 ? index : 'No winner found with that ticket number.' //aqui validamos que el index sea diferente de -1 y que nos envie un mensaje de tiket no encontrado
 }
 
-function displayWinnerInformation (winner) {
-  if (winner !== undefined && winner != null && winner !== 'No winner found with that name.') {
+function displayWinnerInformation (winner) { //funcion para mostrar el ganador 
+  if (winner !== undefined && winner != null && winner !== 'No winner found with that name.') { //aqui se validan que el tiket sea diferente y que efectivamente hay un ganador
     console.log('Winner information: ', winner)
-  } else {
+  } else { //sino envia mensaje de que no es ganador
     console.log('No winner found.')
   }
 }
 
-const winnerByName = findWinnerByName('Emily')
+const winnerByName = findWinnerByName('Manuela')
 const indexWinnerByTicket = findIndexWinnerByTicket(008)
 
 displayWinnerInformation(winnerByName)
